26fc0ef350ca4d217680b9af8cb9518ca89a1b12
dragunwf <plarisanmarc@gmail.com>
2022-11-13 12:12:53 +0800

    Added help function and optimized Run-Bot()

--- Profile/profile.ps1
+++ Profile/profile.ps1
@@ -1,6 +1,11 @@
 Write-Output "Welcome back, Great DragunWF of the Philippines.`n"
 Set-Location E:\DevStuff\Repositories
 
+function Help-Profile() {
+    $commands = "  Help-Profile`n  Run-Bot -botIndex"
+    Write-Host "Commands:`n$commands`n"
+}
+
 function Start-Bot($location, $botName) {
     Write-Host "Running $botName Bot...`n"
     Set-Location $location
@@ -10,15 +15,25 @@ function Start-Bot($location, $botName) {
 function Run-Bot($botIndex) {
     $previousDir = Get-Location
 
-    if (1 -eq $botIndex) {
-        Start-Bot -location "E:\DevStuff\Repositories\Ancalagon" -botName "Ancalagon"
-    }
-    if (2 -eq $botIndex) {
-        Start-Bot -location "E:\DevStuff\Repositories\Scarlet" -botName "Scarlet"
-    }
-    if (3 -eq $botIndex) {
-        Start-Bot -location "E:\DevStuff\Repositories\Dragonbot" -botName "Dragon"
+    switch ($botIndex) {
+        1 {
+            Start-Bot -location "E:\DevStuff\Repositories\Ancalagon" -botName "Ancalagon"
+            continue
+        }
+        2 {
+            Start-Bot -location "E:\DevStuff\Repositories\Scarlet" -botName "Scarlet"
+            continue
+        }
+        3 {
+            Start-Bot -location "E:\DevStuff\Repositories\Dragonbot" -botName "Dragon"
+            continue
+        }
+        default {
+            Write-Host "The index $botIndex is invalid!"
+        }
     }
 
     Set-Location = $previousDir
 }
+
+Help-Profile
